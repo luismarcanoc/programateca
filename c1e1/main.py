dc15de7aeae2f7f56f60ff86e4564ea81f21b842
luismarcanoc <97478114+luismarcanoc@users.noreply.github.com>
2022-01-18 15:02:48 -0400

    Update main.py

--- c1e1/main.py
+++ c1e1/main.py
@@ -1,1 +1,6 @@
+#Escribe el codigo debajo de esta linea
+name="luis"
+age=19
+career="Ingenieria de sistemas"
+print(f"Hola Mundo mi nombre es {name} tengo {age} años, estudio {career} en la UNIMET!")
 
